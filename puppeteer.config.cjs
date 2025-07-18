ed1633e520f41007f510c36f46028c3bc9e7bcd1
huru0825 <idokaramanabu2000@yahoo.co.jp>
2025-07-18 22:25:49 +0900

    Delete puppeteer.config.cjs

--- puppeteer.config.cjs
+++
@@ -1,7 +0,0 @@
-// puppeteer.config.js
-const path = require('path');
-
-module.exports = {
-  // プロジェクト直下の .cache/puppeteer を絶対パスで指定
-  cacheDirectory: path.join(process.cwd(), '.cache/puppeteer'),
-};
