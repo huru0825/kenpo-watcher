4b6af2ceee936aa08ea8414bc1d401e38a7bb501
huru0825 <idokaramanabu2000@yahoo.co.jp>
2025-07-18 23:54:52 +0900

    Update launch.js

--- modules/launch.js
+++ modules/launch.js
@@ -1,17 +1,21 @@
-const { CHROME_PATH } = require('./constants');
-const puppeteer = require('puppeteer-extra');
+let puppeteer = require('puppeteer-extra');
+let launchOptions = {
+  headless: 'new',
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-web-security',
+    '--disable-blink-features=AutomationControlled'
+  ]
+};
 
-function launchBrowser() {
-  return puppeteer.launch({
-    headless: 'new',
-    executablePath: CHROME_PATH,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-web-security',
-      '--disable-blink-features=AutomationControlled'
-    ]
-  });
+function setSharedContext(context) {
+  if (context.puppeteer) puppeteer = context.puppeteer;
+  if (context.launchOptions) launchOptions = context.launchOptions;
 }
 
-module.exports = { launchBrowser };
+function launchBrowser() {
+  return puppeteer.launch(launchOptions);
+}
+
+module.exports = { launchBrowser, setSharedContext };
