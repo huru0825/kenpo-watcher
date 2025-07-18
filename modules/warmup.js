113c2eb86621e62d4226cba77145a0127f2a5d09
huru0825 <idokaramanabu2000@yahoo.co.jp>
2025-07-19 01:38:51 +0900

    Update warmup.js

--- modules/warmup.js
+++ modules/warmup.js
@@ -1,8 +1,10 @@
 const { launchBrowser } = require('./launch');
 
 async function warmup() {
+  console.log('✨ Warmup: launching browser to avoid cold start...');
   const browser = await launchBrowser();
   await browser.close();
+  console.log('✨ Warmup completed');
 }
 
 module.exports = { warmup };
