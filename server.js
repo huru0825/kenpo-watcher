63ce3913d0012172a6dc25219760adfaad1895ac
huru0825 <idokaramanabu2000@yahoo.co.jp>
2025-08-02 02:39:41 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection:', reason);
+  process.exit(1);
+});
+
+process.on('uncaughtException', err => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+
 // server.js
 const express = require('express');
 const fs = require('fs');
