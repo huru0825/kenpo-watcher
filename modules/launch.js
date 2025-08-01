9b2eacac55eb8b2e5aa4b0b9180139ebaefc1d76
huru0825 <idokaramanabu2000@yahoo.co.jp>
2025-08-01 23:21:02 +0900

    Update launch.js

--- modules/launch.js
+++ modules/launch.js
@@ -1,17 +1,13 @@
 // modules/launch.js
 
-// puppeteer を再代入できるよう let で宣言
 let puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-
-// ステルスプラグインを有効化
 puppeteer.use(StealthPlugin());
 
-// Puppeteer 起動オプション
 let launchOptions = {
   headless: true,
-  // 環境変数がなければデフォルトパスにある Chrome を使用
-  executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/google-chrome',
+  // ✅ 明示的に実在する Chrome パスを指定
+  executablePath: '/usr/local/bin/google-chrome',
   args: [
     '--no-sandbox',
     '--disable-setuid-sandbox',
@@ -20,13 +16,11 @@ let launchOptions = {
   ]
 };
 
-// 外部から puppeteer や launchOptions を注入できるようにする関数
 function setSharedContext(context) {
   if (context.puppeteer) puppeteer = context.puppeteer;
   if (context.launchOptions) launchOptions = context.launchOptions;
 }
 
-// 実際にブラウザを起動する関数
 function launchBrowser() {
   return puppeteer.launch(launchOptions);
 }
