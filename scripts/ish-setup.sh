3b7eaf50f0972f702904140ad5c00ce2ee5f0aca
huru0825 <idokaramanabu2000@yahoo.co.jp>
2025-08-03 03:07:50 +0900

    Update ish-setup.sh

--- scripts/ish-setup.sh
+++ scripts/ish-setup.sh
@@ -5,8 +5,8 @@ cp /mnt/Documents/id_rsa ~/.ssh/id_rsa
 cp /mnt/Documents/id_rsa.pub ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa
 
-echo "📁 .env を SSH 先にアップロード..."
-scp -i ~/.ssh/id_rsa /mnt/Documents/kenpo-watcher/.env root@38.54.50.200:~/kenpo-watcher/.env
+echo "🛂 .env をリモートに転送..."
+scp -i ~/.ssh/id_rsa /mnt/Documents/kenpo-watcher/.env root@38.54.50.200:/root/kenpo-watcher/.env
 
 echo "📡 SSH 接続とリモート処理を開始..."
 ssh -i ~/.ssh/id_rsa root@38.54.50.200 <<'EOF'
