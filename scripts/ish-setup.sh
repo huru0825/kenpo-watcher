b276f2c8bc90ec40236c3824dff66e44c562516f
huru0825 <idokaramanabu2000@yahoo.co.jp>
2025-08-03 03:52:48 +0900

    Update ish-setup.sh

--- scripts/ish-setup.sh
+++ scripts/ish-setup.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 set -euxo pipefail
 
-echo "🔐 鍵ファイルを配置..."
-cp /mnt/Documents/id_rsa ~/.ssh/id_rsa
-cp /mnt/Documents/id_rsa.pub ~/.ssh/id_rsa.pub
-chmod 600 ~/.ssh/id_rsa
+echo "🔐 鍵を配置"
+cp /mnt/Documents/id_rsa ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
 
-echo "🛂 .env をリモートに転送..."
-scp -P $(grep SSH_PORT .env | cut -d '=' -f2) -i ~/.ssh/id_rsa /mnt/Documents/kenpo-watcher/.env $(grep SSH_USER .env | cut -d '=' -f2)@$(grep REMOTE_HOST .env | cut -d '=' -f2):$(grep REMOTE_DIR .env | cut -d '=' -f2)/.env
+echo "🛂 .env を転送"
+scp -P "$(grep SSH_PORT .env | cut -d '=' -f2)" -i ~/.ssh/id_rsa \
+  /mnt/Documents/kenpo-watcher/.env \
+  "$(grep SSH_USER .env | cut -d '=' -f2)"@"$(grep REMOTE_HOST .env | cut -d '=' -f2)":"$(grep REMOTE_DIR .env | cut -d '=' -f2)/.env"
 
-echo "📡 SSH 接続とリモート処理を開始..."
-ssh -p $(grep SSH_PORT .env | cut -d '=' -f2) -i ~/.ssh/id_rsa $(grep SSH_USER .env | cut -d '=' -f2)@$(grep REMOTE_HOST .env | cut -d '=' -f2) "\
+echo "📡 SSH で Git Pull と Docker を一括実行"
+ssh -p "$(grep SSH_PORT .env | cut -d '=' -f2)" -i ~/.ssh/id_rsa \
+  "$(grep SSH_USER .env | cut -d '=' -f2)"@"$(grep REMOTE_HOST .env | cut -d '=' -f2)" "\
 cd ~/kenpo-watcher && \
 git pull origin main && \
 docker run --rm -it -v \"\$PWD:/app\" -p 10000:10000 --env-file .env kenpo-watcher \
