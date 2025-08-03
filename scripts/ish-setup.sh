3a9b172690eaf63256dd711bbed632b252264d1b
huru0825 <idokaramanabu2000@yahoo.co.jp>
2025-08-03 14:52:52 +0900

    Update ish-setup.sh

--- scripts/ish-setup.sh
+++ scripts/ish-setup.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 set -euxo pipefail
 
+ENV_PATH="/mnt/Documents/.env"
+
 echo "🔐 秘密鍵を配置"
 cp /mnt/Documents/id_rsa ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
 
 # .env から値を事前に抜き出す（何度も grep しない）
-SSH_PORT=$(grep SSH_PORT .env | cut -d '=' -f2)
-SSH_USER=$(grep SSH_USER .env | cut -d '=' -f2)
-REMOTE_HOST=$(grep REMOTE_HOST .env | cut -d '=' -f2)
-REMOTE_DIR=$(grep REMOTE_DIR .env | cut -d '=' -f2)
+SSH_PORT=$(grep SSH_PORT "$ENV_PATH" | cut -d '=' -f2)
+SSH_USER=$(grep SSH_USER "$ENV_PATH" | cut -d '=' -f2)
+REMOTE_HOST=$(grep REMOTE_HOST "$ENV_PATH" | cut -d '=' -f2)
+REMOTE_DIR=$(grep REMOTE_DIR "$ENV_PATH" | cut -d '=' -f2)
 
 echo "🛂 .env を転送"
 scp -P "$SSH_PORT" -i ~/.ssh/id_rsa \
-  /mnt/Documents/.env \
+  "$ENV_PATH" \
   "$SSH_USER@$REMOTE_HOST:$REMOTE_DIR/.env"
 
 echo "📡 SSH 経由で Git Pull & Docker 起動"
