71c47782e44a484cdae541cd527999bf760b6869
huru0825 <idokaramanabu2000@yahoo.co.jp>
2025-08-03 14:57:18 +0900

    Update ish-setup.sh

--- scripts/ish-setup.sh
+++ scripts/ish-setup.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 set -euxo pipefail
 
-ENV_PATH="/mnt/Documents/.env"
+ENV_FILE="/mnt/Documents/kenpo-watcher.env"
 
 echo "🔐 秘密鍵を配置"
 cp /mnt/Documents/id_rsa ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
 
-# .env から値を事前に抜き出す（何度も grep しない）
-SSH_PORT=$(grep SSH_PORT "$ENV_PATH" | cut -d '=' -f2)
-SSH_USER=$(grep SSH_USER "$ENV_PATH" | cut -d '=' -f2)
-REMOTE_HOST=$(grep REMOTE_HOST "$ENV_PATH" | cut -d '=' -f2)
-REMOTE_DIR=$(grep REMOTE_DIR "$ENV_PATH" | cut -d '=' -f2)
+# .env 相当ファイルから事前に値を取得
+SSH_PORT=$(grep SSH_PORT "$ENV_FILE" | cut -d '=' -f2)
+SSH_USER=$(grep SSH_USER "$ENV_FILE" | cut -d '=' -f2)
+REMOTE_HOST=$(grep REMOTE_HOST "$ENV_FILE" | cut -d '=' -f2)
+REMOTE_DIR=$(grep REMOTE_DIR "$ENV_FILE" | cut -d '=' -f2)
 
 echo "🛂 .env を転送"
 scp -P "$SSH_PORT" -i ~/.ssh/id_rsa \
-  "$ENV_PATH" \
+  "$ENV_FILE" \
   "$SSH_USER@$REMOTE_HOST:$REMOTE_DIR/.env"
 
 echo "📡 SSH 経由で Git Pull & Docker 起動"
