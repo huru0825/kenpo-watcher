843c05d5c2777cddd9e294fdac6fb827d948239e
huru0825 <idokaramanabu2000@yahoo.co.jp>
2025-08-03 11:00:43 +0900

    Update ish-setup.sh

--- scripts/ish-setup.sh
+++ scripts/ish-setup.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 set -euxo pipefail
 
-echo "🔐 鍵を配置"
+echo "🔐 秘密鍵を配置"
 cp /mnt/Documents/id_rsa ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
 
-echo "🛂 .env を転送"
-scp -P "$(grep SSH_PORT .env | cut -d '=' -f2)" -i ~/.ssh/id_rsa \
-  /mnt/Documents/kenpo-watcher/.env \
-  "$(grep SSH_USER .env | cut -d '=' -f2)"@"$(grep REMOTE_HOST .env | cut -d '=' -f2)":"$(grep REMOTE_DIR .env | cut -d '=' -f2)/.env"
+# .env から値を事前に抜き出す（何度も grep しない）
+SSH_PORT=$(grep SSH_PORT .env | cut -d '=' -f2)
+SSH_USER=$(grep SSH_USER .env | cut -d '=' -f2)
+REMOTE_HOST=$(grep REMOTE_HOST .env | cut -d '=' -f2)
+REMOTE_DIR=$(grep REMOTE_DIR .env | cut -d '=' -f2)
 
-echo "📡 SSH で Git Pull と Docker を一括実行"
-ssh -p "$(grep SSH_PORT .env | cut -d '=' -f2)" -i ~/.ssh/id_rsa \
-  "$(grep SSH_USER .env | cut -d '=' -f2)"@"$(grep REMOTE_HOST .env | cut -d '=' -f2)" "\
-cd ~/kenpo-watcher && \
-git pull origin main && \
-docker run --rm -it -v \"\$PWD:/app\" -p 10000:10000 --env-file .env kenpo-watcher \
+echo "🛂 .env を転送"
+scp -P "$SSH_PORT" -i ~/.ssh/id_rsa \
+  /mnt/Documents/kenpo-watcher/.env \
+  "$SSH_USER@$REMOTE_HOST:$REMOTE_DIR/.env"
+
+echo "📡 SSH 経由で Git Pull & Docker 起動"
+ssh -p "$SSH_PORT" -i ~/.ssh/id_rsa "$SSH_USER@$REMOTE_HOST" "\
+  cd $REMOTE_DIR && \
+  git pull origin main && \
+  docker build -t kenpo-watcher . && \
+  docker run --rm -it -p 10000:10000 --env-file .env kenpo-watcher \
 "
